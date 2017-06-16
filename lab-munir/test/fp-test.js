92ad4dfd6085e5d406a5a28669c45d2d2de899ac
Munir Ibrahim <mibrah04@gmail.com>
2017-06-16 12:01:16 -0700

    wrote tests for all the methods

--- lab-munir/test/fp-test.js
+++ lab-munir/test/fp-test.js
@@ -4,37 +4,46 @@ const
   expect = require('expect'),
   fp = require('../lib/fp.js');
 
-let MAP_TEST = { original: [1, 2, 3, 4, 5], result: [2, 4, 6, 8, 10] };
 
 describe('fp call methods', () => {
 
+  // TEST MAP
   describe('test map using call', () => {
     it('should return all idx * 2', () => {
-      let results = fp.mapCall([1, 2, 3, 4, 5], n => n * 2);
+      let results = fp.map([1, 2, 3, 4, 5], n => n * 2);
       expect(results).toEqual([2, 4, 6, 8, 10]);
     });
   });
 
-  describe('test map using apply', () => {
-    it('should return all idx * 2', () => {
-      let results = fp.mapApply(MAP_TEST.original, [n => n * 2]);
-      expect(results).toEqual(MAP_TEST.result);
-    });
-  });
-
-describe('fp filter methods', () => {
-
+  // TEST FILTER
   describe('test filter using call', () => {
     it('should return all idx * 2', () => {
-      let results = fp.map(MAP_TEST.original, [n => n * 2]);
-      expect(results).toEqual(MAP_TEST.result);
+      let results = fp.filter([1, 2, 3, 4, 5, 6], n => n % 2 === 0);
+      expect(results).toEqual([2, 4, 6]);
     });
   });
 
-	describe('test map using apply', () => {
+  // TEST REDUCE
+  describe('test reduce using call', () => {
     it('should return all idx * 2', () => {
-      let results = fp.mapApply(MAP_TEST.original, [n => n * 2]);
-      expect(results).toEqual(MAP_TEST.result);
+      let results = fp.reduce([1, 2, 3, 4, 5], (a, c) => a + c);
+      expect(results).toEqual(15);
     });
+  });
+
+  // TEST SPLICE
+  describe('test splice using apply', () => {
+    it('should return everything after the 4 index', () => {
+      let results = fp.splice([1, 2, 3, 4, 5, 6, 7, 8, 9], [(4)]);
+      expect(results).toEqual([5, 6, 7, 8, 9]);
+    });
+  });
+
+  describe('test concat using apply', () => {
+    it('should return two list combined', () => {
+      let results = fp.concat([1, 2, 3], [4, 5, 6]);
+      expect(results).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+  });
 
 });
